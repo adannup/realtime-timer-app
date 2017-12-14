55b0c4bf493d8fa16f08779f78810f5b999da9dd
adannup <zad3life@gmail.com>
2017-12-13 21:30:38 -0600

    Add start/stop functionalities

--- api.js
+++ api.js
@@ -11,12 +11,51 @@ const DATA_PATH = path.join(__dirname, 'data/data.json');
 api.use(bodyParser.json());
 api.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-api.get('/timers', (req, res) => {
-  const timersJson = utilsTimers.fetchTimers();
-  res.status(200).send(timersJson);
+api
+
+api.post('/timers/start', (req, res) => {
+  const timerId = req.body.id;
+  const now = Date.now();
+
+  fs.readFile(DATA_PATH, (err, data) => {
+    const timers = JSON.parse(data);
+    timers.forEach(timer => {
+      if(timer.id === timerId) {
+        timer.runningSince = now;
+      }
+    });
+
+    fs.writeFile(DATA_PATH, JSON.stringify(timers, null, 4), () => {
+      res.status(200).send({});
+    })
+  })
+});
+
+api.post('/timers/stop', (req, res) => {
+  const timerId = req.body.id;
+  const now = Date.now();
+
+  fs.readFile(DATA_PATH, (err, data) => {
+    const timers = JSON.parse(data);
+    timers.forEach(timer => {
+      if(timer.id === timerId) {
+        const lastElapsed = now - timer.runningSince;
+        timer.elapsed = timer.elapsed + lastElapsed;
+        timer.runningSince = null;
+      }
+    });
+
+    fs.writeFile(DATA_PATH, JSON.stringify(timers, null, 4), () => {
+      res.status(200).send({});
+    })
+  })
 });
 
 api.route('/timers/')
+  .get((req, res) => {
+    const timersJson = utilsTimers.fetchTimers();
+    res.status(200).send(timersJson);
+  })
   .post((req, res) => {
     const newTimer = {
       title: req.body.title,
